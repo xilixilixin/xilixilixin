26d735d7cedf6e20f4754bb891ecc8be4af1b09d
xicoxico <3431803886@qq.com>
2022-08-28 17:09:03 +0800

    项目初始化

---
+++ docs/.vuepress/.temp/internal/siteData.js
@@ -0,0 +1,14 @@
+export const siteData = JSON.parse("{\"base\":\"/xilixili/\",\"lang\":\"en-US\",\"title\":\"XiLiXiLi\",\"description\":\"XiLi小站，专注编程知识分享\",\"head\":[],\"locales\":{}}")
+
+if (import.meta.webpackHot) {
+  import.meta.webpackHot.accept()
+  if (__VUE_HMR_RUNTIME__.updateSiteData) {
+    __VUE_HMR_RUNTIME__.updateSiteData(siteData)
+  }
+}
+
+if (import.meta.hot) {
+  import.meta.hot.accept(({ siteData }) => {
+    __VUE_HMR_RUNTIME__.updateSiteData(siteData)
+  })
+}
